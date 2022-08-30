7a6c13db9c5b071ffe5d047a56aa1b3b2cb61f36
Thyago Machado <thymac@gmail.com>
2022-08-30 08:01:45 -0300

    starting project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex w="100vw" h="100vh" align={"center"} justify="center">
+      <Flex as={"form"}>H1 Center</Flex>
+    </Flex>
+  );
+}
